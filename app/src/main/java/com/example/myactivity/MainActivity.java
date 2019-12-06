32cc19ab9fd1d98145bc333cc1b12655a72f99cc
majidalykhan <majidalykhan@gmail.com>
2019-12-06 16:25:43 +0500

    Design

--- app/src/main/java/com/example/myactivity/MainActivity.java
+++ app/src/main/java/com/example/myactivity/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.myactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView appTitle, finishTasks, nomoreTasks;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        appTitle = findViewById(R.id.Apptitle);
+        finishTasks = findViewById(R.id.finishTasks);
+        nomoreTasks = findViewById(R.id.nomoretasks);
+
+
     }
 }
